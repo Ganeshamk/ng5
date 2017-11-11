0e5bc14c75f63d2faafe0bcea67e528f5e29559c
ganesh <ganesh@tapontech.com>
2017-11-11 15:42:10 +0530

    first commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class AboutComponent implements OnInit {
+  goals: any;
+  
+  constructor(private route: ActivatedRoute, private router: Router,private _data: DataService) { 
+    this.route.params.subscribe(res => console.log(res.id));
+  }
+
+  ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
+  }
+
+  sendMeHome() {
+    this.router.navigate(['/']);
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo: '/', pathMatch:'full' },
+  { path:'', component: HomeComponent },
+  { path:'about/:id', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
